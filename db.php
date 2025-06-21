dc9fd843cda6df67c50309999fca23559ca65c60
Reazul Hoque <engrreaz@gmail.com>
2025-06-21 18:52:22 +0600

    Progga

--- db.php
+++ db.php
@@ -10,6 +10,6 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 $conn->set_charset("utf8");
-// include '../db.php';
+// include '../db.php';......
 
 require_once 'utils.php';
